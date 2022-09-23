af0c7c554428a6439974c442d274bbaee1c9dc5c
RyanWangCs <106635779+RyanWangCs@users.noreply.github.com>
2022-09-22 17:17:13 -0700

    a

---
+++ Grass.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("Attack"):
+		var GrassEffect = load("res://GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
+		queue_free()
